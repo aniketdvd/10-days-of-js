aa83a9a12eb1ce7abb25d7a557893f607167d2c5
aniketdvd <dwi.aniket@gmail.com>
2019-08-10 22:14:28 +0530

    Day 1 - let and const

---
+++ day1/letAndConst.js
@@ -0,0 +1,7 @@
+const PI = Math.PI;
+// Print the area of the circle:
+let inp = readLine();
+let r = parseFloat(inp);
+console.log(PI * r * r);
+// Print the perimeter of the circle:
+console.log(2 * PI * r);
