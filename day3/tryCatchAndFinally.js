c68f36e49073de1c4a50723a3c360a691fac3a4f
aniketdvd <dwi.aniket@gmail.com>
2019-08-10 22:23:55 +0530

    Day 3 - Try&Catch

---
+++ day3/tryCatchAndFinally.js
@@ -0,0 +1,13 @@
+/*
+ * Complete the reverseString function
+ * Use console.log() to print to stdout.
+ */
+function reverseString(s) {
+    try {
+        console.log(s.split('').reverse().join(''));
+    }
+    catch (err) {
+        console.log(err.message);
+        console.log(s);
+    }
+}
